a243e81799e4dec0847891f10c38f4743c074c0a
Kalong Habit <45380323+kalong-habit@users.noreply.github.com>
2023-01-29 14:50:56 +0700

    Create 4-the-bresenham.cpp

---
+++ 4-the-bresenham.cpp
@@ -0,0 +1,14 @@
+void bresenhamLine(int x0, int y0, int x1, int y1) {
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = x0 < x1 ? 1 : -1;
+    int sy = y0 < y1 ? 1 : -1;
+    int err = (dx > dy ? dx : -dy) / 2;
+
+    while(true) {
+        setPixel(x0, y0); // Function to set the pixel at (x0, y0)
+        if (x0 == x1 && y0 == y1) break;
+        int e2 = err;
+        if (e2 > -dx) { err -= dy; x0 += sx; }
+        if (e2 < dy) { err += dx; y0 += sy; }
+    }
